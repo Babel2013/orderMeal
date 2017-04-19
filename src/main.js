a43cec7711f4f1e9c46be12fdd25489dfff82791
shenzuojun <shenzuojun@Joe.local>
2017-04-19 09:47:36 +0800

    new

--- src/main.js
+++ src/main.js
@@ -2,10 +2,32 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import axios from 'axios';
+import VueRouter from 'vue-router';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.prototype.$http = axios;
+
+const routes = [
+  {path: '/', component: goods},
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
+];
+
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  router,
+  render: h => h(App)
 });
